2ba18265dd98d0099b4ade5384ee55c2b17be72c
he-codes-25 <sohumgarg14@gmail.com>
2026-04-01 10:42:54 -0700

    Time: 0 ms (100%), Space: 19.1 MB (92.47%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def alternateDigitSum(self, n: int) -> int:
+        s=0
+        n=str(n)
+        for i in range(len(n)):
+            if i%2==0:
+                s+=int(n[i])
+            else:
+                s+=-int(n[i])
+        return s
+            

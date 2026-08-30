2c4c5f884644f2e6b2b7d1f22e6a8e6735038f40
he-codes-25 <sohumgarg14@gmail.com>
2026-08-30 03:12:28 -0700

    Time: 1 ms (96.77%), Space: 12.3 MB (90.34%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,8 +1,4 @@
 class Solution(object):
     def isPalindrome(self, x):
-        x=str(x)
-        reverse=''
-        for i in range(len(x)-1, -1, -1):
-            reverse += x[i]
-        return x==reverse
+        return str(x)==str(x)[::-1]
         
